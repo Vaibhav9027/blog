724adf5332783a92da655b7b20e019744c801623
Vaibhav9027 <vaibhavgarg9520@gmail.com>
2024-09-20 19:24:06 +0530

    Initial commit

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+
+
+class Config:
+
+	SECRET_KEY='Dev'
+	SQLALCHEMY_DATABASE_URI='sqlite:///blog_database.db'
+	MAIL_SERVER='smtp.googlemail.com'
+	MAIL_PORT=587
+	MAIL_USE_TLS=True
+	MAIL_USERNAME='abc@gmail.com'
+	MAIL_PASSWORD='xyz'
